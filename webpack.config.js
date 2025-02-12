8d8246cbd71a4092f59c584aa152a00aaa0d8fc1
panik <panikadak@gmail.com>
2025-02-13 00:06:16 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.[contenthash].js',
+    publicPath: './',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      three: path.resolve('./node_modules/three')
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: 'css',
+          to: 'css'
+        },
+        { 
+          from: 'js',
+          to: 'js'
+        },
+        { 
+          from: 'img',
+          to: 'img'
+        },
+        {
+          from: 'assets',
+          to: 'assets'
+        }
+      ]
+    })
+  ]
+};
